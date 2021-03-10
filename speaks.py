502466b694421e16704e32726724f6e49e574df3
Liz-Mary <maybe.rt@yandex.ru>
2021-03-10 18:38:40 +0300

    add search in youtube, add fix weather

--- speaks.py
+++ speaks.py
@@ -11,10 +11,12 @@ from nltk import tokenize
 from time import strftime
 from pyowm.owm import OWM
 from pyowm.utils.config import get_default_config
+import settings
 
 
 reco = sr.Recognizer() 
 micro = sr.Microphone(sample_rate = 48000, device_index=0, chunk_size = 1024) 
+
 
 def hello():
     print()
@@ -64,24 +66,23 @@ def say_print():
         # использование online-распознавания через Google 
         recognized_data = reco.recognize_google(audio, language='ru_RU')
         text_command = recognized_data.lower()
-        #text_commands = text_command.split(' ')
         print(f'Вы сказали: {text_command}')
         say(f'Вы сказали' + text_command)
+        textopen = text_command.split(' ')[-1]
         if 'помощь' in text_command:
             time.sleep(2)
             help()
         elif 'открой' in text_command or 'найди' in text_command:
-            textopen = text_command.split(' ')[-1]
             open_search_google(textopen)
             #time.sleep(1)
             return say_print()
+        elif 'видео' in text_command:
+            open_search_youtube(textopen)
         elif 'wiki' in text_command or 'вики' in text_command or 'википедия' in text_command:
-            textopen = text_command.split(' ')[-1]
             wiki(textopen)
-        elif 'который час' in text_command or 'время' in text_command:
+        elif 'который час' in text_command or 'время' in text_command or 'дата' in text_command:
             colortime()
         elif 'погода' in text_command:
-            textopen = text_command.split(' ')[-1]
             get_weather(textopen)
         elif 'до связи' in text_command or 'до свидания' in text_command:
             time.sleep(2)
@@ -94,6 +95,15 @@ def say_print():
         print('Я Вас не поняла, повторите')
         say('Я Вас не поняла, повторите')
         
+def open_search_youtube(textopen):   
+    # 
+    # открыввем гугл (реализовать открытие любой ссылки позже) 
+    #
+    print(f'Открываю на ютубе {textopen}')
+    time.sleep(2)
+    say('Открываю ютуб')
+    subprocess.getoutput("google-chrome-stable https://youtube.com/results?search_query="+textopen) 
+
 def open_search_google(textopen):   
     # 
     # открыввем гугл (реализовать открытие любой ссылки позже) 
@@ -102,6 +112,7 @@ def open_search_google(textopen):
     time.sleep(2)
     say('Открываю')
     subprocess.getoutput("google-chrome-stable https://google.com/search?q="+textopen) #как реализовать открытие в разных браузерах?
+
 
 def wiki(textopen):
     #
@@ -133,18 +144,21 @@ def get_weather(textopen):
     config_dict = get_default_config()
     config_dict['language'] = 'ru' 
     try:
-        owm = OWM('d40c1a8c5f52d5af0bad7ddb8508a452', config_dict)
+        owm = OWM(settings.API, config_dict)
         mgr = owm.weather_manager()
         observation =  mgr.weather_at_place(textopen)
         w = observation.weather
         temper = w.temperature('celsius')
-        print ('Средняя температура. В настоящее время: ', int(temper['temp'])) 
-        print ('Максимальная температура: ', int(temper['temp_max'])) 
-        print ('Минимальная темература: ', int(temper['temp_min']))
-        say('Средняя темепература за день:', (str(int(temper['temp']))))
-        time.sleep(7)
-    except (AttributeError,ValueError):
+        temper0, temper_max, temper_min = str(int(temper['temp'])), int(temper['temp_max']), int(temper['temp_min'])
+        print('Средняя температура. В настоящее время: ', temper0) 
+        print('Максимальная температура: ', temper_max) 
+        print('Минимальная темература: ', temper_min)
+        time.sleep(2)
+        say(f'Средняя темепература за день:' + temper0)
+        time.sleep(2)
+    except:
         pass
+
 
 if __name__ == '__main__':
    
